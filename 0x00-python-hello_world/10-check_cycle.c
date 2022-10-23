9cd40bc9d53b03034ea38c77ef0caf910e509151
Mkhuseli Faku <fakugmkhuseli@gmail.com>
2022-10-23 03:07:19 -0700

    10. Linked list cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -15,7 +15,7 @@ int check_cycle(listint_t *list)
 	if (!list)
 		return (0);
 
-	while (slow && fast fast ->next)
+	while (slow && fast && fast->next)
 	{
 		slow = slow->next;
 		fast = fast->next->next;
