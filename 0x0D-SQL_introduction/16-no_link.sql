5869af9e8c174cf4e839b5dbf8aa3eb493c265a7
Mkhuseli Faku <fakugmkhuseli@gmail.com>
2022-11-01 07:04:53 -0700

    16. Say my name

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,7 @@
+-- Lists all records of the table "second_table" of the database "hbtn_0c_0" in the MySQL server.
+-- Results should display the score and the name (in this order)
+-- Records should be listed by descending score
+SELECT `score`, `name`
+FROM `second_table`
+WHERE `name` != ""
+ORDER BY `score` DESC;
