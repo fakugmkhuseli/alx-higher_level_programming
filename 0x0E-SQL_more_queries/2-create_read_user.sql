30afcf67436a55ec2019d013bf5f0693a7b61d32
Mkhuseli Faku <fakugmkhuseli@gmail.com>
2022-11-02 05:48:38 -0700

    2. Read user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,12 +1,12 @@
 -- Creates the database "hbtn_0d_2" and the user "user_0d_2".
 -- "user_0d_2" should have only SELECT privilege in the database "hbtn_0d_2".
 CREATE DATABASE
-       IF NOT EXISTS `hbtn_0d_2`;
+    IF NOT EXISTS `hbtn_0d_2`;
 CREATE USER
-       IF NOT EXISTS 'user_0d_2'@'localhost'
-       IDENTIFIED BY 'user_0d_2_pwd';
+    IF NOT EXISTS 'user_0d_2'@'localhost'
+    IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT
-       ON `hbtn_0d_2`.*
-       TO 'user_0d_2'@'localhost'
-       IDENTIFIED BY 'user_0d_2_pwd';
+    ON `hbtn_0d_2`.*
+    TO 'user_0d_2'@'localhost'
+    IDENTIFIED BY 'user_0d_2_pwd';
 FLUSH PRIVILEGES;
