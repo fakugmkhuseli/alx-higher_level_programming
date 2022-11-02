9a774bbaa56dbe34eac7e6e0562348107c2e3080
Mkhuseli Faku <fakugmkhuseli@gmail.com>
2022-11-02 04:14:21 -0700

    11. Genre ID for all shows

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,8 +1,8 @@
 -- Lists all shows contained in the database "hbtn_0d_tvshows".
 -- Each record should display: tv_shows.title - tv_show_genres.genre_id.
 -- Results are sorted in ascending order by tv_shows.title and tv_show_genres.genre_id.
-SELECT s.`title`. g.`genre_id`
+SELECT s.`title`, g.`genre_id`
   FROM `tv_shows` AS s
        LEFT JOIN `tv_show_genres` AS g
        ON s.`id` = g.`show_id`
- ORDER BY s.`title`, g.`genre_id`;
+ORDER BY s.`title`, g.`genre_id`;
