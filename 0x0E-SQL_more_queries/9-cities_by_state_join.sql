653b0d18ed2368ca115fdf5f5ddd8982b03fef5a
Mkhuseli Faku <fakugmkhuseli@gmail.com>
2022-11-02 03:18:20 -0700

    9. Cities by States

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,8 +1,8 @@
 -- Lists all cities contained in the database "hbtn_0d_usa".
 -- Each record should display: cities.id - cities.name - states.name
 -- Results are sorted in ascending order by cities.id.
-SELECT a.`id`, a.`name`, b.`name`
-FROM `cities` AS a
-INNER JOIN `states` AS b
-ON a.`states_id` = b.`id`
-ORDER BY a.`id`;
+SELECT c.`id`, c.`name`, s.`name`
+FROM `cities` AS c
+INNER JOIN `states` AS s
+ON c.`state_id` = s.`id`
+ORDER BY c.`id`;
