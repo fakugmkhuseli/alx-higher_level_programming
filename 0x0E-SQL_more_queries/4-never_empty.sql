9aa266519f60703781e5c2867c60446dd60f7952
Mkhuseli Faku <fakugmkhuseli@gmail.com>
2022-11-02 01:33:16 -0700

    4. ID can't be null

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,3 @@
+-- Creates the table id_not_null on the MySQL server.
+-- If the table id_not_null already exists,the script should not fail.
+CREATE TABLE IF NOT EXISTS `id_not_null` (`id` INT DEFAULT 1, `name` VARCHAR(256);
