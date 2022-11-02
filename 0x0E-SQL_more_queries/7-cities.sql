8ecfb3b318c2cb39f46eba9753d70ae0668cb741
Mkhuseli Faku <fakugmkhuseli@gmail.com>
2022-11-02 03:21:29 -0700

    7. Cities table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,6 +1,6 @@
 -- Creates the database "hbtn_0d_usa" and the table "cities" (in the database hbtn_0d_usa) on the MySQL server.
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
-CREATE TABLE IF NOT EXISTS `cities`.`hbtn_0d_usa` (
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
     PRIMARY KEY(`id`), 
     `id` INT NOT NULL AUTO_INCREMENT,
     `state_id` INT NOT NULL,
