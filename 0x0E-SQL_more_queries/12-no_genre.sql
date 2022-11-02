741745a23baa366cc1f3cfbeff4629e5632110a1
Mkhuseli Faku <fakugmkhuseli@gmail.com>
2022-11-02 04:21:08 -0700

    12. No genre

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,9 @@
+-- Lists all shows contained in "hbtn_0d_tvshows" without a genre linked.
+-- Each record should display: tv_shows.title - tv_show_genres.genre_id.
+-- Results are sorted in ascending order by tv_shows.title and tv_show_genres.genre_id.
+SELECT s.`title`, g.`genre_id`
+  FROM `tv_shows` AS s
+       LEFT JOIN `tv_show_genres` AS g
+       ON s.`id` = g.`show_id`
+       WHERE g.`genre_id` IS NULL
+ORDER BY s.`title`, g.`genre_id`;
