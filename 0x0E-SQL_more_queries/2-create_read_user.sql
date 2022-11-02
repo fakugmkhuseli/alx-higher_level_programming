6a728d9e1e1ad9401c8c9830d10c5bce7caa7b97
Mkhuseli Faku <fakugmkhuseli@gmail.com>
2022-11-02 03:44:59 -0700

    2. Read user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -5,7 +5,7 @@ CREATE USER
     IF NOT EXISTS 'user_0d_2'@'localhost'
     IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT
-   ON `hbtn_0d_2`.*
-   TO 'user_0d_2'@'localhost'
-   IDENTIFIED BY 'user_0d_2_pwd';
+    ON `hbtn_0d_2`.*
+    TO 'user_0d_2'@'localhost'
+    IDENTIFIED BY 'user_0d_2_pwd';
 FLUSH PRIVILEGES;
