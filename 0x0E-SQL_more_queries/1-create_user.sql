a5671cd2c5f92db0623d4f9ac3af0129e64af54e
Mkhuseli Faku <fakugmkhuseli@gmail.com>
2022-11-02 05:04:20 -0700

    1. Root user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,10 @@
 -- Creates the MySQL server user "user_od_1".
 -- "user_0d_1" should have all privileges on your MySQL server.
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER
+       IF NOT EXISTS 'user_0d_1'@'localhost'
+       IDENTIFIED WITH 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES
+       ON *.*
+       TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
 
