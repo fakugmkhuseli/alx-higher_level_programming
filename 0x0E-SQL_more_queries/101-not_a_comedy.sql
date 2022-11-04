06bce71c46d4868feb177986634d099f246a79b0
Mkhuseli Faku <fakugmkhuseli@gmail.com>
2022-11-04 02:03:04 -0700

    18. No Comedy tonight!

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,20 @@
+-- Lists all shows without the genre Comedy in the database hbtn_0d_tvshows.
+-- The tv_genres table contains only one record where name = Comedy.
+-- Each record should display: tv_shows.title.
+SELECT DISTINCT `title`
+FROM `tv_shows` AS t
+LEFT JOIN `tv_show_genres` AS s
+ON s.`show_id` = t.`id`
+
+LEFT JOIN `tv_genres` AS g
+ON g.`id` = s.`genre_id`
+WHERE t.`title` NOT IN
+      (SELECT `title`
+	FROM `tv_shows` AS t
+	      INNER JOIN `tv_show_genres` AS s
+	      ON s.`show_id` = t.`id`
+
+	      INNER JOIN `tv_genres` AS g
+	      ON g.`id` = s.`genre_id`
+	      WHERE g.`name` = "Comedy")
+ORDER BY `title`
