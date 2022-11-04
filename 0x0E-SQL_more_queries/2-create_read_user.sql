201f5f888630221c128eb6eb6f797bbd67a84a4e
Mkhuseli Faku <fakugmkhuseli@gmail.com>
2022-11-04 01:30:23 -0700

    2. Read user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,5 +2,4 @@
 -- The "user_0d_2" should have only SELECT privilege in the database "hbtn_0d_2".
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-FLUSH PRIVILEGES;
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
