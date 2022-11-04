04ff02ac1fa45aa163e0cb5db30b4207947e6b08
Mkhuseli Faku <fakugmkhuseli@gmail.com>
2022-11-04 02:14:46 -0700

    17. Not my genre

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -7,9 +7,9 @@ INNER JOIN `tv_show_genres` AS s
 ON g.`id` = s.`genre_id`
 
 INNER JOIN `tv_shows` AS t
-ON s.`show-id` = t.`id`
+ON s.`show_id` = t.`id`
 WHERE g.`name` NOT IN
-      (SELECT `name
+      (SELECT `name`
 	FROM `tv_genres` AS g
 	     INNER JOIN `tv_show_genres` AS s
 	     ON g.`id` = s.`genre_id`
