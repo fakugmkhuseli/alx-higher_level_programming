e917d8f682f0f9b0ae5a4431b1d567ec88000839
Mkhuseli Faku <fakugmkhuseli@gmail.com>
2022-11-21 00:32:36 -0800

    7. Update dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,5 +2,5 @@
 
 def update_dictionary(a_dictionary, key, value):
     """Replaces or add key/value in a dictionary."""
-    a_dictionary['key'] = value
+    a_dictionary[key] = value
     return (a_dictionary)
