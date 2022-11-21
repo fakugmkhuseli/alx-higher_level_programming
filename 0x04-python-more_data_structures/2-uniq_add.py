3086e0b1a9f681405e53637940d0fc2ff79dc015
Mkhuseli Faku <fakugmkhuseli@gmail.com>
2022-11-21 02:29:25 -0800

    2. Unique addition

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-
 def uniq_add(my_list=[]):
     """Adds all unique integers in a list."""
-    result = 0
-    for x in set(my_list):
-        result += x
-        return (result)
+    uniq_list = set(my_list)
+    num = 0
+
+    for i in uniq_list:
+        num += 1
+
+    return (num)
