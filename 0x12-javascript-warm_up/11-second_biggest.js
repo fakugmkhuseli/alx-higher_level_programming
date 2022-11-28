149891645ca516c87d763aa13bafab386cfbe38b
Mkhuseli Faku <fakugmkhuseli@gmail.com>
2022-11-28 02:49:16 -0800

    11. Second biggest!

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@ if (process.argv.length <= 3) {
   console.log(0);
 } else {
   const args = process.argv.map(Number)
-    .slide(2, process.argv.length)
+    .slice(2, process.argv.length)
     .sort((a, b) => a - b);
  console.log(args[args.length - 2]);
 }
