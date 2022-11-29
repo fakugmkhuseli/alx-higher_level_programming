bf211a818b0f1f4555e860809c6b70add0d7b1d5
Mkhuseli Faku <fakugmkhuseli@gmail.com>
2022-11-29 04:37:25 -0800

    10. Number conversion

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,2 +1,7 @@
 #!/usr/bin/node
-exports.converter = function (base) { return num => num.toString(base): };
+
+exports.converter = function (base) { 
+  return function (num) {
+    return num.toString(base);
+  }
+};
