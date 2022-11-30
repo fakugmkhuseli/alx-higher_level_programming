ab2e25ac91632bde2e47d1373bbdbbba443474f6
Mkhuseli Faku <fakugmkhuseli@gmail.com>
2022-11-29 22:58:10 -0800

    6. Square #1

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,15 +1,14 @@
 #!/usr/bin/node
-const SquareP = require('./5-square.js');
+const SquareP = require('./5-square');
 
 class Square extends SquareP {
   charPrint (c) {
     if (c === undefined) {
       this.print();
     } else {
-      for (let i = 0; i < this.height; i++)
+      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
     }
-    console.log(c.repeat(this.width));
   }
 }
-
+  
 module.exports = Square;
