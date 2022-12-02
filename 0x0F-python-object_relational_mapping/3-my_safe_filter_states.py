974ebfcdcff10c76ab9ad229901b3aac79400fe6
Mkhuseli Faku <fakugmkhuseli@gmail.com>
2022-12-02 05:24:43 -0800

    SQL Injection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-""" Takes in arguments and displays all values in the states table of hbtn_0e_0_usa
-    where name matches the argument."""
+""" Takes in arguments and displays all values in the states table
+    of hbtn_0e_0_usa where name matches the argument."""
 import MySQLdb
 import sys
 
