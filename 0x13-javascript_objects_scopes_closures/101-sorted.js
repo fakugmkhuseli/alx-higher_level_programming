3bbfaed47f5b12adddea91b3c04532bcea08800b
Mkhuseli Faku <fakugmkhuseli@gmail.com>
2022-12-02 03:16:54 -0800

    12. Sorted occurences

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const dict = require('./101-data.js').dict;
+const dict = require('./101-data').dict;
 let newDict = {};
 for (let key in dict) {
   if (newDict[dict[key]] === undefined) {
