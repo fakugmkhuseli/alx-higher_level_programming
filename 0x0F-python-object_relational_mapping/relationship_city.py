5275f7063168e14790c6a7227e5575217f7d86e2
Mkhuseli Faku <fakugmkhuseli@gmail.com>
2022-12-04 04:53:57 -0800

    City class

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Defines a City model."""
 
-from model_state import Base
+from relationship_state import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
