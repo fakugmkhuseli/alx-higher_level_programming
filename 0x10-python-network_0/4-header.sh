304fd45a82a50ebfb6ab17ebf65226f0817506c9
Mkhuseli Faku <fakugmkhuseli@gmail.com>
2022-12-15 06:08:22 -0800

    cURL headers

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Takes in a URL as an argument, sends a GET request to the URL,
-# and displays the body of the response.
+# Takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response.
 curl -sH "X-HolbertonSchool-User-Id: 98" "${1}"
