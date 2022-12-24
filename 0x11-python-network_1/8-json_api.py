cee5e43b6defe2c476eea039bad8569569c32ca9
Mkhuseli Faku <fakugmkhuseli@gmail.com>
2022-12-24 01:27:12 -0800

    Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Sends a POST request to http://0.0.0.0:5000/search_user with a given letter."""
+"""Sends a POST request to http://0.0.0.0:5000/search_user with a given
+   letter."""
 import sys
 import requests
 
