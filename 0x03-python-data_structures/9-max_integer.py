ca1f49956c61e1f93610aef42b30be46b8c6aae9
Mkhuseli Faku <fakugmkhuseli@gmail.com>
2022-12-24 02:43:07 -0800

    Finds the biggest integer of a list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,7 +4,7 @@
 def max_integer(my_list=[]):
     """Finds the biggest integer of a list."""
     if len(my_list) == 0:
-        return None
+        return (None)
 
     biggest = my_list[0]
     for i range(len(my_list)):
