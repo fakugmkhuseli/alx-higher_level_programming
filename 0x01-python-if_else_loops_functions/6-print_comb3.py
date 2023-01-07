ffe5e403c2e404e097f1fb36174a4cf3247b5db5
Mkhuseli Faku <fakugmkhuseli@gmail.com>
2023-01-07 01:43:46 -0800

    Prints all possible different combinations of two digits.

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""Prints all possible different combinations of two digits."""
+for dig1 in range(0, 10):
+    for dig2 in range(dig1 + 1, 10):
+        if dig1 == 8 and dig2 == 9:
+            print("{}{}".format(dig1, dig2))
+        else:
+            print("{}{}".format(dig1, dig2), end=", ")
