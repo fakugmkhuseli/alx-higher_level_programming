8aae87a9ad6ef3e586b1fd563d8f513642109f38
Mkhuseli Faku <fakugmkhuseli@gmail.com>
2023-01-07 02:30:52 -0800

    Prints the ASCII alphabet, in reverse order

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-"""Prints the ASCII alphabet, in reverse order, alternating lowercase and uppercase."""
+"""Prints the ASCII alphabet, in reverse order."""
 i = 0
 for c in range(ord('z'), ord('a') - 1, -1):
     print("{}".format(chr(c - i)), end="")
