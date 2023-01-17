b54a47f4b3a71ac7837822581d8046e5afbbd870
Mkhuseli Faku <fakugmkhuseli@gmail.com>
2023-01-17 13:52:45 +0000

    clear nbkjmklca
    vim 4-starwars_count.js/
    
    vim 4-starwars_count.js
    
    q
    q/q
    quit
    
    vim 4-starwars_count.js
    
    ]

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,7 +4,7 @@ request(process.argv[2], function (error, response, body) {
   if (!error) {
     count results = JSON.parse(body).results;
     console.log(results.reduce((count, movie) => {
-      return movie.characters.find((character) => character.endswith('/18/'))
+      return movie.characters.find((character) => character.endsWith('/18/'))
         ? count + 1
         : count;
     }, 0));
