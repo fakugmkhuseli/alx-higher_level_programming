731cbe63e38412e09056b17ead480d97cc24c79b
Mkhuseli Faku <fakugmkhuseli@gmail.com>
2023-01-17 13:37:23 +0000

    Gets the contents of a webpage and stores it in a file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 const fs = require('fs');
 const request = require('request');
-request(process.argv[2]).pipe(fs.createwriteStream(process.argv[3]));
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
